bc5f10821841bde4b0e4c8ffa814039e70a5b1f8
shehab2049 <123630817+shehab2049@users.noreply.github.com>
2026-08-12 12:36:01 +0300

    Update app.py

--- Default Project/app.py
+++ Default Project/app.py
@@ -1,6 +1,6 @@
 import importlib.util
 import sys
-
+from Default Project.web_app import app
 # Load web_app.py from the folder with a space in its name
 spec = importlib.util.spec_from_file_location("web_app", "Default Project/web_app.py")
 module = importlib.util.module_from_spec(spec)
@@ -9,3 +9,4 @@ spec.loader.exec_module(module)
 
 # Expose the Flask app for Vercel
 app = module.app
+
